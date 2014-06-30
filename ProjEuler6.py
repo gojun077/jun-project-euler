08f94860746235bb525798a0a4f9279130a99634
archjun <gojun077@gmail.com>
2014-06-30 23:06:35 +0900

    Added doctest to diffSquare and added docstring; removed debugging print stmts

--- ProjEuler6.py
+++ ProjEuler6.py
@@ -1,18 +1,28 @@
 # Project Euler Problem 6
-# Find the difference between the sum of the squares of the first 
+# Find the difference between the sum of the squares of the first
 # 100 natural numbers and the square of the sum.
 
+import doctest
+
 def diffSquare(n):
-	sum_sq = 0
-	sum1 = 0
-	for i in range(1, n + 1):
-		sum_sq += i**2
-		sum1 += i
-	sq_sum = sum1**2
-	print(sum_sq)
-	print(sq_sum)
-	return (sq_sum - sum_sq)
+    '''
+    Int -> Int
 
-#print(diffSquare(10)) # test case should return 385, 3025, 2640
+    Return difference between the sum of the squares of the first n
+    natural numbers and the square of the sum of the first n natural
+    numbers
+
+    >>> diffSquare(10)
+    2640
+    '''
+    sum_sq = 0
+    sum1 = 0
+    for i in range(1, n + 1):
+        sum_sq += i**2
+        sum1 += i
+        sq_sum = sum1**2
+    return (sq_sum - sum_sq)
+
+doctest.testmod()
+
 print(diffSquare(100))
-
