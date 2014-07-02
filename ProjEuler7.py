e25cbfeba6a35486c132e2885e0d588465f9ffb6
archjun <gojun077@gmail.com>
2014-07-02 10:15:02 +0900

    We refactored the generator so that it will continue to yield primes without limit. We also added the function nth_prime(nth) which calls genPrimes() as a helper function when calculating the nth prime.
    Our result for nth_prime(10001) is correct.

--- ProjEuler7.py
+++ ProjEuler7.py
@@ -2,28 +2,51 @@
 # By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can
 # see that the 6th prime is 13. What is the 10,001st prime number?
 
+import doctest
+
 def genPrimes():
-	'''uses a generator to create a sequence of primes'''
-	yield 2
-	yield 3
-	primes_list = [2, 3]
-	final = primes_list[-1]
+    '''
+    -> Int
 
-    # tests 100000 numbers for primality starting from 3
-	for i in range(100000):
-		final += 2
-		for e in primes_list:
-			if final % e == 0:
-				break
-		# loop fell through without finding a factor
-		else:
-			# add new prime to list
-			primes_list.append(final)
-			yield final
+    Use a generator to create primes which are memoized
+    in a list
+    '''
+    yield 2
+    yield 3
+    primes_list = [2, 3]
+    last = primes_list[-1]
 
-counter = 0
-for i in genPrimes():
-	print i
-	counter += 1
-	if counter == 10001:
-		break
+    while True:
+        last += 2
+        for p in primes_list:
+            if last % p == 0:
+                break
+        # loop falls through without finding a factor
+        else:
+            primes_list.append(last)
+            yield last
+
+def nth_prime(nth):
+    '''
+    Int -> Int
+
+    Return the nth prime
+
+    >>> nth_prime(1)
+    2
+
+    >>> nth_prime(2)
+    3
+
+    >>> nth_prime(10)
+    29
+    '''
+    counter = 0
+    for i in genPrimes():
+        counter += 1
+        if counter == nth:
+           return i
+
+doctest.testmod()
+
+nth_prime(10001)
